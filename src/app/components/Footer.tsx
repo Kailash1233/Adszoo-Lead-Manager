458d196f828bac889d88e864e19312108acfbd7b
Kailash <kailash61203@gmail.com>
2024-07-19 00:54:04 +0530

    added status and follow up format

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,14 @@ const Footer = () => {
     <footer className="bg-gray-800 p-4 text-white mt-4">
       <div className="container mx-auto text-center">
         &copy; {new Date().getFullYear()} Adszoo Lead Manager. All rights
-        reserved.
+        reserved. Developed by{" "}
+        <a
+          href="https://the-k-innovations.vercel.app/"
+          className="text-blue-200"
+          target="_blank"
+        >
+          @Adszoo
+        </a>
       </div>
     </footer>
   );
--- src/app/components/LeadList.tsx
+++ src/app/components/LeadList.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, SetStateAction, SetStateAction } from "react";
 import { db } from "../firebaseConfig";
 import {
   collection,
@@ -15,7 +15,10 @@ import {
   FaInstagram,
   FaFacebook,
   FaLinkedin,
+  FaCheck,
+  FaTimes,
 } from "react-icons/fa";
+import { format } from "date-fns";
 
 const LeadList = () => {
   const [leads, setLeads] = useState([]);
@@ -32,14 +35,19 @@ const LeadList = () => {
     instagram: "",
     facebook: "",
     linkedin: "",
+    status: "", // New status field
   });
   const [editingLeadId, setEditingLeadId] = useState(null);
+  const [confirmDeleteId, setConfirmDeleteId] = useState(null);
 
   useEffect(() => {
     const fetchLeads = async () => {
       const leadsCollection = await getDocs(collection(db, "leads"));
       setLeads(
-        leadsCollection.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        leadsCollection.docs.map((doc: { data: () => any; id: any }) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
       );
     };
     fetchLeads();
@@ -60,15 +68,35 @@ const LeadList = () => {
       instagram: "",
       facebook: "",
       linkedin: "",
+      status: "", // Clear status after adding
     });
     // Fetch leads again to update the list
     const leadsCollection = await getDocs(collection(db, "leads"));
     setLeads(
-      leadsCollection.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      leadsCollection.docs.map((doc: { data: () => any; id: any }) => ({
+        ...doc.data(),
+        id: doc.id,
+      }))
     );
   };
 
-  const handleEditLead = (lead) => {
+  const handleEditLead = (
+    lead: SetStateAction<{
+      name: string;
+      companyName: string;
+      service: string;
+      budget: string;
+      comments: string;
+      mobile: string;
+      followUpDate: string;
+      leadSource: string;
+      email: string;
+      instagram: string;
+      facebook: string;
+      linkedin: string;
+      status: string;
+    }>
+  ) => {
     setNewLead(lead);
     setEditingLeadId(lead.id);
   };
@@ -89,28 +117,91 @@ const LeadList = () => {
       instagram: "",
       facebook: "",
       linkedin: "",
+      status: "", // Clear status after updating
     });
     setEditingLeadId(null);
     // Fetch leads again to update the list
     const leadsCollection = await getDocs(collection(db, "leads"));
     setLeads(
-      leadsCollection.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      leadsCollection.docs.map((doc: { data: () => any; id: any }) => ({
+        ...doc.data(),
+        id: doc.id,
+      }))
     );
   };
 
-  const handleDeleteLead = async (id) => {
-    const leadDoc = doc(db, "leads", id);
-    await deleteDoc(leadDoc);
+  const handleDeleteLead = async (id: SetStateAction<null>) => {
+    setConfirmDeleteId(id); // Set the id to confirm deletion
+  };
+
+  const confirmDeleteLead = async () => {
+    await deleteDoc(doc(db, "leads", confirmDeleteId));
+    setConfirmDeleteId(null); // Reset confirm delete id
     // Fetch leads again to update the list
     const leadsCollection = await getDocs(collection(db, "leads"));
     setLeads(
-      leadsCollection.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      leadsCollection.docs.map((doc: { data: () => any; id: any }) => ({
+        ...doc.data(),
+        id: doc.id,
+      }))
+    );
+  };
+
+  const cancelDeleteLead = () => {
+    setConfirmDeleteId(null); // Reset confirm delete id without deleting
+  };
+
+  const toggleStatus = async (leadId: any) => {
+    const lead = leads.find((lead) => lead.id === leadId);
+    const updatedStatus = lead.status === "complete" ? "" : "complete";
+    await updateDoc(doc(db, "leads", leadId), { status: updatedStatus });
+    // Fetch leads again to update the list
+    const leadsCollection = await getDocs(collection(db, "leads"));
+    setLeads(
+      leadsCollection.docs.map((doc: { data: () => any; id: any }) => ({
+        ...doc.data(),
+        id: doc.id,
+      }))
+    );
+  };
+
+  const [systemDate, setSystemDate] = useState(new Date()); // State to hold current system date
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSystemDate(new Date()); // Update system date every second (can be adjusted)
+    }, 1000);
+
+    return () => clearInterval(interval); // Cleanup interval
+  }, []);
+
+  // Function to check if followUpDate matches current system date
+  const isSameDateAsSystem = (followUpDate: string | number | Date) => {
+    const formattedFollowUpDate = new Date(followUpDate);
+    return (
+      formattedFollowUpDate.getDate() === systemDate.getDate() &&
+      formattedFollowUpDate.getMonth() === systemDate.getMonth() &&
+      formattedFollowUpDate.getFullYear() === systemDate.getFullYear()
     );
   };
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Lead List</h1>
+      <div
+        className="w-auto h-[40vh] flex items-center justify-center relative"
+        style={{
+          background:
+            "url(/images/serviceHero.jpg) no-repeat center center fixed",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="relative text-center text-white mx-24">
+          <h1 className="text-4xl py-8 font-bold">
+            Transform Leads into Loyal Clients – Effortlessly!
+          </h1>
+        </div>
+      </div>
       <div className="mt-8 bg-gray-100 p-6 rounded-lg shadow-md text-black">
         <h2 className="text-xl font-bold mb-4 text-center text-gray-700">
           Add / Update Lead
@@ -228,7 +319,7 @@ const LeadList = () => {
           ) : (
             <button
               onClick={handleAddLead}
-              className="bg-green-500 text-white px-6 py-3 rounded-lg shadow-md hover:bg-green-600 transition duration-300 ease-in-out"
+              className="bg-blue-500 text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-600 transition duration-300 ease-in-out"
             >
               Add Lead
             </button>
@@ -236,6 +327,7 @@ const LeadList = () => {
         </div>
       </div>
 
+      <h2 className="text-2xl font-bold mt-8 mb-4">Leads</h2>
       <table className="min-w-full bg-white mt-6 rounded-lg shadow-md overflow-hidden">
         <thead className="bg-gray-800 text-white">
           <tr>
@@ -249,6 +341,7 @@ const LeadList = () => {
             <th className="py-3 px-4 text-sm md:text-base">Lead Source</th>
             <th className="py-3 px-4 text-sm md:text-base">Email</th>
             <th className="py-3 px-4 text-sm md:text-base">Socials</th>
+            <th className="py-3 px-4 text-sm md:text-base">Status</th>
             <th className="py-3 px-4 text-sm md:text-base">Actions</th>
           </tr>
         </thead>
@@ -276,7 +369,11 @@ const LeadList = () => {
               <td className="border-t px-4 py-2 text-sm md:text-base">
                 {lead.mobile}
               </td>
-              <td className="border-t px-4 py-2 text-sm md:text-base">
+              <td
+                className={`border-t px-4 py-2 text-sm md:text-base ${
+                  isSameDateAsSystem(lead.followUpDate) ? "bg-green-300" : ""
+                }`}
+              >
                 {lead.followUpDate}
               </td>
               <td className="border-t px-4 py-2 text-sm md:text-base">
@@ -285,36 +382,49 @@ const LeadList = () => {
               <td className="border-t px-4 py-2 text-sm md:text-base">
                 {lead.email}
               </td>
-              <td className="border-t px-4 py-2 text-sm md:text-base flex justify-center items-center space-x-2">
+              <td className="border-t px-4 py-2 text-sm md:text-base">
                 {lead.instagram && (
                   <a
-                    href={lead.instagram}
+                    href={`https://www.instagram.com/${lead.instagram}`}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-pink-500"
+                    className="text-gray-600 hover:text-gray-900 transition duration-300"
                   >
                     <FaInstagram />
                   </a>
                 )}
                 {lead.facebook && (
                   <a
-                    href={lead.facebook}
+                    href={`https://www.facebook.com/${lead.facebook}`}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-blue-700"
+                    className="text-gray-600 hover:text-gray-900 transition duration-300"
                   >
                     <FaFacebook />
                   </a>
                 )}
                 {lead.linkedin && (
                   <a
-                    href={lead.linkedin}
+                    href={`https://www.linkedin.com/in/${lead.linkedin}`}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-blue-500"
+                    className="text-gray-600 hover:text-gray-900 transition duration-300"
                   >
                     <FaLinkedin />
                   </a>
+                )}
+              </td>
+              <td className="border-t px-4 py-2 text-sm md:text-base">
+                {lead.status === "complete" ? (
+                  <FaCheck
+                    className="text-green-500 cursor-pointer"
+                    onClick={() => toggleStatus(lead.id)}
+                  />
+                ) : (
+                  <FaTimes
+                    className="text-red-500 cursor-pointer"
+                    onClick={() => toggleStatus(lead.id)}
+                  />
                 )}
               </td>
               <td className="border-t px-4 py-2 flex justify-center items-center space-x-1 md:space-x-2">
@@ -335,6 +445,31 @@ const LeadList = () => {
           ))}
         </tbody>
       </table>
+
+      {/* Confirmation Modal */}
+      {confirmDeleteId && (
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center">
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+            <p className="text-xl text-gray-800 mb-4">
+              Are you sure you want to delete this lead?
+            </p>
+            <div className="flex justify-center space-x-4">
+              <button
+                onClick={confirmDeleteLead}
+                className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 transition duration-300"
+              >
+                Yes
+              </button>
+              <button
+                onClick={cancelDeleteLead}
+                className="bg-gray-300 text-gray-800 px-4 py-2 rounded-lg hover:bg-gray-400 transition duration-300"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
